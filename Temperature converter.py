7fd9268b8614b5ab3fe7d06b660872e2d2a4384e
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-02 05:16:47 -0800

    Refactor temperature converter with comments
    Added comments for clarity and improved code structure.

--- Temperature converter.py
+++ Temperature converter.py
@@ -1,7 +1,12 @@
+#python based temp converter
+#input temp
 temp = float(input("enter temperature:"))
+#input unit
 unit  = input("C OR F:").upper()
 
+#conditions
 if(unit == "C"):
     print((temp * 9/5) + 32)
 else:
+
     print((temp - 32) * 5/9)
